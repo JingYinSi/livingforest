87489d4bb596958219c1c7aedd08f8eeec167fd6
clx <hbdennischen@gmail.com>
2021-05-03 18:01:27 +0800

    ui

--- pages/lesson/lesson.js
+++ pages/lesson/lesson.js
@@ -4,21 +4,21 @@ const app = getApp()
 
 Page({
   data: {
-    banner: '../../imgs/gongxiu.jpg',
+    banner: '../../imgs/chizhou.jpg',
     list: {
       img: '../../imgs/gongke_logo.png',
       items: [
         {
           text: "马金鹏三尊心咒",
-          desc: "开始30天 300人 共修3219399494遍"
+          desc: "开始30天 我 共修3219399494遍"
         },
         {
           text: "普获悉地祈祷文",
-          desc: "开始30天 300人 共修3219399494遍"
+          desc: "开始30天 我 共修3219399494遍"
         },
         {
           text: "莲花生大士师心咒",
-          desc: "开始30天 300人 共修3219399494678904555遍"
+          desc: "开始30天 我 3219399494678904555遍"
         }
       ]
     }
