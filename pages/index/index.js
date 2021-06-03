4ca2292ebcf03aac7fc82a841f3c0d1831ed6e63
clx <hbdennischen@gmail.com>
2021-06-03 16:37:34 +0800

    动态渲染

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,21 +4,33 @@ const app = getApp()
 
 Page({
   data: {
-    upPic: "../../imgs/上部.png",
-    activaties: {
-      items: [
-        {
-          text: "萨嘎月",
-          selected: true
-        },
-        {
-          text: "百日共修"
-        },
-        {
-          text: "日常"
-        }
-      ]
-    }
+    card: "../../data/lotus.jpg",
+    activaties: [
+      {
+        text: "萨嘎月",
+        selected: true
+      },
+      {
+        text: "百日共修"
+      },
+      {
+        text: "日常"
+      }
+    ],
+    lessons: [
+      {
+        text: "莲师",
+        selected: true
+      },
+      {
+        text: "文殊"
+      },
+      {
+        text: "马金"
+      }
+    ],
+    title: "莲花生大士心咒",
+    subtitle: "每日需完成3000遍"
   },
   imageError: function(e) {
     console.log('image3发生error事件，携带值为', e.detail.errMsg)
